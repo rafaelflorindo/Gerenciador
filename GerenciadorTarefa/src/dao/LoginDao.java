610892a1778ac4a2160eb764470153f22f7aac41
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2025-08-14 21:24:32 -0300

    Add files via upload

---
+++ GerenciadorTarefa/src/dao/LoginDao.java
@@ -0,0 +1,26 @@
+package dao;
+
+import model.Login;
+
+public class LoginDao {
+
+    public void cadastrar(Login login){
+
+    }
+
+    public void listar(){
+
+    }
+
+    public void buscarPorId(int id){
+
+    }
+
+    public void atualizar(Login login){
+
+    }
+
+    public void atualizarStatus(Login login, Boolean ativo){
+
+    }
+}
---
+++ GerenciadorTarefa/src/model/Login.java
@@ -0,0 +1,93 @@
+package model;
+
+public class Login {
+    public int id;
+    public String nome;
+    public String email;
+    public String senha;
+    public boolean ativo;
+    public String dataCadastro;
+    public String dataAtualizacao;
+
+    public Login() {
+    }
+
+    public Login(int id, String nome, String email, String senha, boolean ativo, String dataCadastro, String dataAtualizacao) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.ativo = ativo;
+        this.dataCadastro = dataCadastro;
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Login{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                ", ativo=" + ativo +
+                ", dataCadastro='" + dataCadastro + '\'' +
+                ", dataAtualizacao='" + dataAtualizacao + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public String getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(String dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(String dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+}
